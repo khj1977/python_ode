cf65cacb13225ff83a23a709c80057ad3a9fc9ff
Hwi Jun KIM <euler.bonjour@gmail.com>
2022-04-11 18:57:39 +0900

    some change for init point and f.

--- test-ode-batch.py
+++ test-ode-batch.py
@@ -3,13 +3,15 @@ import batch
 import sys
 import matplotlib.pyplot as plt
 import numpy as np
+import math
 
 class TestODEBatch(batch.Batch):
   # def __init__
   def __init__(self):
     # ODEOneDimEulerMethod:
     # def __init__(self, deltaT, startT, endT, startX, startXDot):
-    self.odeEngine = ode_euler.ODEOneDimEulerMethod(0.01, 0, 100, 10.0, 1.0)
+    self.odeEngine = ode_euler.ODEOneDimEulerMethod(0.01, 0, 100, 0.05, 0.05)
+    # self.odeEngine = ode_euler.ODEOneDimEulerMethod(0.01, 0, 100, 0.1, 0.1)
 
 
   def solve(self, f):
@@ -40,7 +42,9 @@ class TestODEBatch(batch.Batch):
 
 # ddx = f(t, x, xDot)
 # f = lambda t, x, xDot: -1.0 * x - 0.5 * xDot
-f = lambda t, x, xDot: -1.0 * x - 0.5 * xDot + 2.0 + xDot**3
+# f = lambda t, x, xDot: -1.0 * x - 0.5 * xDot - 2.0 * xDot**3
+# f = lambda t, x, xDot: - 6.0 * x - 5.0 * xDot - 10.0 * t * x
+f = lambda t, x, xDot: - (6.0 + math.sin(t)) * x - (5.0 + math.cos(t)) * xDot - (2.0 * math.sin(t)) * x
 # f = lambda t, x, xDot: -2.0 * x - 1.0 * xDot + 3.0 * np.cos(x)
 # f = lambda t, x, xDot: -2.0 * x - 1.0 * xDot
 # return (resultT, resultX, resultXDot)
