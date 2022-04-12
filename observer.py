b060eb95a94e614ffe6a02a0386f1751cea3db5f
Hwi Jun KIM <euler.bonjour@gmail.com>
2022-04-12 13:24:25 +0900

    raw sketch of observer.

--- observer.py
+++ observer.py
@@ -1,12 +1,31 @@
+from operator import truediv
+
+
 class Observer:
-  def __init__(self, odeEngin, f, bx0, bxDot0, be0):
-    self.odeEngine = odeEngine
-    self.orgODE = f
-    self.bx0 = x0
-    self.bxDot0 = xDot0
-    self.e0 = e0
+  def __init__(self, odeEngineReal, f_ode, odeEngineObserver, f_observer, f_control_observer, bx0, bxDot0, be0):
+    self.odeEngineReal = odeEngineReal
+    self.odeEngineObserver = odeEngineObserver
+    self.orgODE = f_ode
+    self.orgDEObserver = f_observer
+    self.orgControlObserver = f_control_observer
+    self.bx0 = bx0
+    self.bxDot0 = bxDot0
+    self.e0 = be0
     self.bresultT = []
     self.bresultX = []
     self.bresultXDot = []
 
   def solve(self):
+    t = 0.0
+    e = be0
+    while t > self.endT:
+      nominalData = self.odeEngineReal.solve(self.orgODE)
+      observerData = self.odeEngineObserver.solve(
+        self.orgODEObserver, 
+        self.orgControlObserver
+        )
+
+      # feedforward observer and data.
+      # errorData = nominalData - observerData (and feedforward) 
+      # control input = K(t) * error
+      # yield estimated uncertainty.
