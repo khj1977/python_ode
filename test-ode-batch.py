7e72fbaf35a7ab663c56d60b2dea2c6e5de91dfd
Hwi Jun KIM <euler.bonjour@gmail.com>
2022-04-23 12:26:03 +0900

    visualization of control input has been added but still there is bug.

--- test-ode-batch.py
+++ test-ode-batch.py
@@ -20,6 +20,8 @@ class TestODEBatch(batch.Batch):
     self.odeEngine = ode_euler.ODEOneDimEulerMethod(deltaT, staetT, endT, startX, startXDot, f, env, envT, ode_control_input.ControlInput())
     self.observerEngine = ode_euler.ODEOneDimEulerMethod(deltaT, start, endT, startX + 2.0, startXDot + 2.0, f, observerEnvX, envT, controlInput)
     # self.odeEngine = ode_euler.ODEOneDimEulerMethod(0.01, 0, 100, 0.1, 0.1)
+    self.controlInput = controlInput
+
     self.resultT = []
     self.resultXDot = []
     self.resultX = []
@@ -30,6 +32,9 @@ class TestODEBatch(batch.Batch):
 
     self.errorResultXDot = []
     self.errorResultX = []
+
+    self.controlResultXDot = []
+    self.controlResultX = []
 
   def solve(self):
     # self.resultT, self.resultX, self.resultXDot = self.odeEngine.solve(f)
@@ -54,6 +59,9 @@ class TestODEBatch(batch.Batch):
       self.errorResultX.append(errorDynamics.getX1())
       self.errorResultXDot.append(errorDynamics.getX2())
       # end of debug
+
+      self.controlResultX.append(self.controlInput.getControlInput())
+      self.controlResultXDot.append(self.controlInput.getControlInputDot())
     # end of debug
 
   def saveToFile(self):
@@ -70,6 +78,7 @@ class TestODEBatch(batch.Batch):
     plt.plot(self.resultXDot, self.resultX, label="actual system")
     plt.plot(self.observerResultXDot, self.observerResultX, label="observer")
     plt.plot(self.errorResultXDot, self.errorResultX, label="error")
+    plt.plot(self.controlResultXDot, self.controlResultX, label="control")
 
     # 凡例の表示
     plt.legend()
@@ -83,9 +92,9 @@ class TestODEBatch(batch.Batch):
 # f = lambda t, x, xDot: -1.0 * x - 5.0 * xDot - 2.0 * xDot**3
 # f = lambda t, x, xDot: -6.0 * x - 5.0 * xDot - 1.0 * xDot**3
 # f = lambda t, x, xDot: - 6.0 * x - 5.0 * xDot - 10.0 * t * x
-f = lambda t, x, xDot: - (6.0 + math.sin(t)) * x - (5.0 + math.cos(t)) * xDot - (2.0 * math.sin(t)) * x
+# f = lambda t, x, xDot: - (6.0 + math.sin(t)) * x - (5.0 + math.cos(t)) * xDot - (2.0 * math.sin(t)) * x
 # f = lambda t, x, xDot: -2.0 * x - 1.0 * xDot + 3.0 * np.cos(x)
-# f = lambda t, x, xDot: -2.0 * x - 1.0 * xDot
+f = lambda t, x, xDot: -2.0 * x - 1.0 * xDot
 # return (resultT, resultX, resultXDot)
 env = ode_env.ODEEnv()
 envObserver = ode_env.ODEEnv()
