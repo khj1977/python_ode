7207fb3973e155c9d3a5cde65367bfb9eb61dd29
Hwi Jun KIM <euler.bonjour@gmail.com>
2022-04-24 13:43:55 +0900

    dynamics of disturbance has been debugged.

--- ode_disturbance.py
+++ ode_disturbance.py
@@ -3,21 +3,33 @@ class Disturbance:
         self.eq = eq
         self.envX = envX
         self.envT = envT
+        self.disturbance = 0.0
+        self.disturbanceDot = 0.0
+        self.prevDisturbance = 0.0
 
     # debug
     # implement the following method.
-    def getDynamics(self):
+    def calcDynamics(self):
         self.x = self.envX.getX()
         self.xDot = self.envX.getXDot()
         t = self.envT.getT()
 
-        self.xddot = self.eq(t, self.x, self.xDot)
+        self.disturbance = self.eq(t, self.x, self.xDot)
 
-        return self.xddot
+        return self.disturbance
     # end of debug
+
+    def calcDisturbanceDot(self):
+        self.prevDisturbance = self.disturbance
+        self.calcDynamics()
+        diff = self.disturbance - self.prevDisturbance
+        deltaT = self.envT.getDeltaT()
+        disturbanceDot = diff / deltaT
+        self.disturbanceDot = disturbanceDot
+        return self
 
     def getStates(self):
         # debug
         # the following is mistake. Implment appropriate one
-        return [self.envT.getT(), self.x, self.xDot]
+        return [self.envT.getT(), self.disturbance, self.disturbanceDot]
         # end of debug
--- ode_euler.py
+++ ode_euler.py
@@ -69,7 +69,7 @@ class ODEOneDimEulerMethod:
     # ODE
     # Hack the following to use multiple number of f()s to handle observer.
     # make t, x, xdot env to enclose to handle multple phase env namely: PhaseEnv
-    ddx = self.diffEQ(t, x, xDot) + self.controlInput.getControlInput() + self.disturbance.getDynamics()
+    ddx = self.diffEQ(t, x, xDot) + self.controlInput.getControlInput() + self.disturbance.calcDynamics()
     # ddx = self.diffEQ(t, x, xDot)
     # end of ODE
 
