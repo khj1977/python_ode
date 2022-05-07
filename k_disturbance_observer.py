80ee6b91dec4ba31eb94720cacf74202c2eed168
Hwi Jun KIM <euler.bonjour@gmail.com>
2022-05-07 13:32:45 +0900

    Draft of disturbance observer has been made. Test later on.

--- k_disturbance_observer.py
+++ k_disturbance_observer.py
@@ -6,18 +6,56 @@ import ode_euler
 import ode_control_input
 
 class KDisturbanceObserver:
-    def __init__(self, odeEngineReal, envT, f, startX, startXDot):
+    def __init__(self, odeEngineReal, controlInput, envT, f, startX, startXDot):
         self.odeEngineReal = odeEngineReal
         self.envT = envT
         self.states = ODEEnv()
         self.f = f
-        self.controlInput = ControlInput()
-        self.odeEngineFF = ode_euler.ODEOneDimEulerMethod(envT.getDeltaT(), envT.getStartT(), envT.getEndT(), startX, startXDot, self.f, self.states, self.nvT, ode_control_input.ControlInput())
+        self.controlInput = controlInput
+
+        self.odeEngineFF = ode_euler.ODEOneDimEulerMethod(envT.getDeltaT(), envT.getStartT(), envT.getEndT(), startX, startXDot, self.f, self.states, self.envT, controlInput)
+        
         #  def __init__(self, xEnv1, xEnv2, envT):
+        # r_bDot = Ar_b + Bp
         self.modifiedSignalDynamics = ErrorDynamics(self.odeEngineReal.getStates(), self.odeEngineFF.getStates(), self.envT)
 
+        # disturbance observer follows modified reference signal.
+        # u = K(t) * e(t) where e(t)  = x - r_b where r_b = self.modifiedSignalDynamics
+        # errorDynamics = error_dynamics.ErrorDynamics(env, envObserver, envT)
+        # controlInput.setState(errorDynamics)
+        self.controlInput.setState(self.modifiedSignalDynamics)
+
+        self.disturbanceObserverEngine = ode_euler.ODEOneDimEulerMethod(env.getDeltaT, envT.getStartT(), envT.getEndT(), startX, startXDOt, self.f, self.states, envT, self.controlInput)
+
+
     def inc(self):
+        self.calcControlInput()
+        self.applyControlInput(self.controlInput)
+        result = self.xinc()
+
+        return result
+
+    def xinc(self):
         # debug
-        # implement the following
-        return self
+        # control input to applied to observer
+        # odeEngineRelal is inc()ed by another method or scope.
+        result = self.disturbanceObserverEngine.inc()
         # end of debug
+
+        return result
+
+    # debug
+    # implement the following method
+    def calcControlInput(self):
+        # return self
+        self.controlInput.getStates().calcErr()
+        self.controlInput.getStates().calcErrDot()
+        self.controlInput.calcControlInput()
+        # end of debug
+
+    # debug
+    # implement the following method
+    # this may not br required since control is set to ode engine.
+    def applyControlInput(self, controlInput):
+        return self
+    # end of debug
