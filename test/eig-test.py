5442107242231fc70773eac36a6fb58faafd552f
Hwi Jun KIM <euler.bonjour@gmail.com>
2022-05-16 14:57:10 +0900

    eig is minus but which is not eig designed. If it were fixed, it would work. I suppose.

--- test/eig-test.py
+++ test/eig-test.py
@@ -1,16 +1,21 @@
 from numpy import linalg as la
 import numpy as np
 
-a21 = -6.0
-a22 = -5.0
-k1 = -1.0 * (10.0 + 20.0) + 6.0
-k2 = -1.0 * (10.0 * 20.0) + 5.0
+#a21 = -6.0
+#a22 = -5.0
+a21 = -2.0
+a22 = -1.0
+k1 = -1.0 * (10.0 + 20.0) - a21
+k2 = -1.0 * (10.0 * 20.0) - a22
 a = np.array([[0., 1.], [a21 + k1, a22 + k2]])
 w, v = la.eig(a)
 print(w)
 
-w, v = la.eig(np.array([[0.0, 1.0], [-6.0, -5.0]]))
+w, v = la.eig(np.array([[0.0, 1.0], [a21, a22]]))
 print(w)
 
-w, v = la.eig(np.array([[0.0, 1.0], [-60.0, -50.0]]))
-print(w)
+#w, v = la.eig(np.array([[0.0, 1.0], [-6.0, -5.0]]))
+#print(w)
+
+#w, v = la.eig(np.array([[0.0, 1.0], [-60.0, -50.0]]))
+#print(w)
