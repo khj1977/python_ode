c281c9406f76fac05c51ac5e4fbf57aeddadf01e
Hwi Jun KIM <euler.bonjour@gmail.com>
2023-10-15 19:38:41 +0900

    analogue signal class has partially been impled.

---
+++ ode_analogue_sin_signal.py
@@ -0,0 +1,19 @@
+class ODEAnalogueSineSignal:
+    def __init__(self):
+        self.lambda1 = 0.0
+        self.tau = 0.0
+        self.t = 0.0
+        set.envT = none
+        # return self
+
+    def setEnvT(self, envT):
+        self.envT = envT
+        return self
+
+    def reset(self):
+        self.tau = 0.0
+
+        return self
+    
+    def isSin(self):
+        pass
