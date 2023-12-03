ee2a3506ce92f1d1ce33facb0005c805e0fd4e91
Hwi Jun KIM <euler.bonjour@gmail.com>
2023-12-03 11:21:34 +0900

    determiner for delta lambda has been added.

---
+++ ode_delta_lambda.py
@@ -0,0 +1,9 @@
+class DeltaLambda:
+
+    # debug
+    # choose f or g based on lyapunov value. set omega and delta.
+    # set tau as actual num which is current time for start.
+    # capule those info to objects and make method lambdaUncrementer->exec()
+    # end of debug
+    def __init__(self):
+        pass
