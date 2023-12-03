c5127587c6cca766d9db59e17d673f55477d64ef
Hwi Jun KIM <euler.bonjour@gmail.com>
2023-12-03 17:58:24 +0900

    some part of delta lambda has been impled.

--- ode_delta_lambda.py
+++ ode_delta_lambda.py
@@ -1,3 +1,5 @@
+import math
+
 class DeltaLambda:
 
     # debug
@@ -5,13 +7,23 @@ class DeltaLambda:
     # set tau as actual num which is current time for start.
     # capule those info to objects and make method lambdaUncrementer->exec()
     # end of debug
-    def __init__(self, envT):
+    def __init__(self, envT, errorDynamics, delta, omega, tau):
         self.envT = envT
+        self.errorDynamics = errorDynamics
+
+        self.setDelta(delta)
+        self.setOmega(omega)
+        self.setTau(tau)
 
         self.calcF = lambda x: x
-        self.calcG = lambda x: x + 1
-
-        delta = self.getDelta()
+        def calcG(t): 
+            if (t <= math.pi / omega + tau):
+                1.0 / 2.0 * delta * math.sin(omega * (t - tau) + 3.0/2.0 * math.pi) - 1.0 / 2.0 * delta
+            else:
+                0.0
+        
+        self.calcG = calcG
+       
         self.calcH = lambda x: -1.0 * delta
         self.calcQ = lambda x: 0.0
 
@@ -22,14 +34,20 @@ class DeltaLambda:
     def inc(self):
         pass
 
-    def getDeltaLambda(self);
+    def getDeltaLambda(self):
         pass
 
-    def getLambda(self):
-        pass
+    def setDelta(self, delta):
+        self.delta = delta
+
+        return self
 
     def setOmega(self, omega):
-        pass
+        self.omega = omega
+
+        return self
 
     def setTau(self, tau):
-        pass
+        self.tau = tau
+
+        return self
